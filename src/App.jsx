ec57bc781f0105fbc33f11e94c6dc2e45a7cd098
Edouard <edouardn94@gmail.com>
2023-02-14 10:49:47 +0100

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import styles from './style'
-import Billing from './Components/Billing/Billing'
-import Carddeal from './components/Carddeal/Carddeal'
-import Clients from './components/Clients/Clients'
-import CTA from './components/CTA/CTA'
-import Footer from './components/Footer/Footer'
-import Hero from './Components/Hero/Hero'
-import Navbar from './components/Navbar/Navbar'
-import Stats from './components/Stats/Stats'
-import Testimonials from './components/Testimonials/Testimonials'
-import Business from './components/Business/Business'
+import React from 'react';
+import styles from './style';
+import Billing from './Components/Billing/Billing';
+import Carddeal from './components/Carddeal/Carddeal';
+import Clients from './components/Clients/Clients';
+import CTA from './components/CTA/CTA';
+import Footer from './components/Footer/Footer';
+import Hero from './Components/Hero/Hero';
+import Navbar from './components/Navbar/Navbar';
+import Stats from './components/Stats/Stats';
+import Testimonials from './components/Testimonials/Testimonials';
+import Business from './components/Business/Business';
 
 const App = () =>(
   <div className="bg-primary dark:bg-secondary w-full overflow-hidden">
